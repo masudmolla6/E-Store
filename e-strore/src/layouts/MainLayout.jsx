f0808d20e5bcaf324cde6248cce88dcdd833ad42
masudmolla6 <masudmolla2937@gmail.com>
2025-10-06 21:56:18 +0600

    first commit

---
+++ e-strore/src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../pages/shared/Navbar/Navbar'
+import { Outlet } from 'react-router'
+import Footer from '../pages/shared/Footer/Footer'
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default MainLayout
---
+++ e-strore/src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router'
+import router from './router/router'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div className='font-urbanist max-w-7xl mx-auto'>
+      <RouterProvider router={router}>
+        
+      </RouterProvider>
+    </div>
+  </StrictMode>,
+)
