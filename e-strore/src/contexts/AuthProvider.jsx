d78518084d3db207f281e7c88aa462568ab34f52
masudmolla6 <masudmolla2937@gmail.com>
2025-10-26 19:45:48 +0600

    try generate jet token

--- e-strore/src/contexts/AuthProvider.jsx
+++ e-strore/src/contexts/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from 'react'
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updateProfile, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import app from '../firebase/firebase.init';
+import useAxiosPublic from '../hooks/useAxiosPublic';
 
 export const AuthContext=createContext(null);
 const auth = getAuth(app);
@@ -9,6 +10,7 @@ const AuthProvider = ({children}) => {
     const [user, setUser]=useState(null);
     const [loading, setLoading]=useState(true);
     const provider = new GoogleAuthProvider();
+    const axiosPublic=useAxiosPublic();
 
     const createUser=(email, password)=>{
         setLoading(true);
@@ -37,7 +39,17 @@ const AuthProvider = ({children}) => {
     useEffect(()=>{
         const unsubscribe=onAuthStateChanged(auth, curretUser =>{
             setUser(curretUser);
-            console.log(curretUser);
+            // console.log(curretUser);
+            const userInfo={email:curretUser?.email};
+            if(curretUser){
+                axiosPublic.post("/jwt", userInfo)
+                .then(res=>{
+                    console.log(res.data)
+                })
+            }
+            // else{
+
+            // }
             setLoading(false);
         });
         return ()=>{
