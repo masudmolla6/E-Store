90616c8204488fa59c60d8d3f046d891b0b8b11f
masudmolla6 <masudmolla2937@gmail.com>
2025-10-26 20:35:32 +0600

    save token in localstorage

--- e-strore/src/contexts/AuthProvider.jsx
+++ e-strore/src/contexts/AuthProvider.jsx
@@ -44,12 +44,17 @@ const AuthProvider = ({children}) => {
             if(curretUser){
                 axiosPublic.post("/jwt", userInfo)
                 .then(res=>{
-                    console.log(res.data)
+                    // console.log(res.data)
+                    if(res.data.token){
+                        localStorage.setItem("access-token", res.data.token);
+                        setLoading(false);
+                    }
                 })
             }
-            // else{
-
-            // }
+            else{
+                localStorage.removeItem("access-token");
+                setLoading(false);
+            }
             setLoading(false);
         });
         return ()=>{
