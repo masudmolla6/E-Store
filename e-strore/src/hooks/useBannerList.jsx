060f8f5dc0089e39adb413c85a325bbed12531a3
masudmolla6 <masudmolla2937@gmail.com>
2026-03-22 11:28:55 +0600

    updated use Banner list hook

--- e-strore/src/hooks/useBannerList.jsx
+++ e-strore/src/hooks/useBannerList.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import useAxiosPublic from './useAxiosPublic';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
+import useAxiosPublic from "./useAxiosPublic";
 
 const useBannerList = () => {
-    const axiosPublic=useAxiosPublic();
+  const axiosPublic = useAxiosPublic();
 
-    const {data:banners=[], isPending:loading, error, refetch}=useQuery({
-      queryKey:["banners"],
-      queryFn:async()=>{
-          const res=await axiosPublic.get("/banners");
-          return res.data;
-      }
-    })
-    return [banners,loading, error, refetch]
+  const {
+    data: banners = [],
+    isPending: loading,
+    error,
+    refetch,
+  } = useQuery({
+    queryKey: ["banners"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/banners");
+      return res.data;
+    },
+    staleTime: 1000 * 60,
+  });
+
+  return [banners, loading, error, refetch];
 };
 
 export default useBannerList;
